2c867e47bd68d210f28f78ce3733f3a3db4ff710
Mihnea Mihai <mihnea.mihai@gmx.com>
2023-10-05 23:46:52 +0300

    Add `form_of` node purging and edge recreation

--- sql/initdb.sql
+++ sql/initdb.sql
@@ -37,7 +37,7 @@ BEGIN
         GET DIAGNOSTICS deleted_rows := ROW_COUNT;
     
         RAISE NOTICE E'\tPurged % entries from core.node of unwanted POSSs',
-            lang_count;
+            deleted_rows;
         ANALYZE;
         COMMIT;
         RAISE NOTICE E'\tExecution time: %', 
@@ -181,7 +181,7 @@ BEGIN
                 SELECT DISTINCT parent_id FROM core.edge
             );
 
-        TRUNCATE TABLE core.node CASCADE;
+        TRUNCATE TABLE core.node;
 
         INSERT INTO core.node
             SELECT * FROM pre.staging_node;
@@ -195,6 +195,104 @@ BEGIN
         RAISE NOTICE E'\tExecution time: %', 
             to_char(clock_timestamp() - start_time, 'MI:SS');
     END;
+
+<<purge_no_value_form_of_nodes>>
+    DECLARE
+        start_time timestamp := clock_timestamp();
+        node_count integer;
+    BEGIN
+        RAISE NOTICE 'PURGING CORE.NODE OF NO VALUE FORM_OF NODES';
+
+        INSERT INTO pre.staging_node
+            SELECT *
+            FROM core.node
+            WHERE node.node_id IN (
+                SELECT parent_id AS del_id FROM core.edge
+                UNION
+                (SELECT child_id AS del_id FROM core.edge 
+                    WHERE edge_type <> 'form_of')
+            );
+
+        TRUNCATE TABLE core.node;
+
+        INSERT INTO core.node
+            SELECT * FROM pre.staging_node;
+        
+        TRUNCATE TABLE pre.staging_node;
+    
+        node_count := (SELECT count(*) FROM core.node);
+        RAISE NOTICE E'\tcore.node now has % entries', node_count;
+        ANALYZE;
+        COMMIT;
+        RAISE NOTICE E'\tExecution time: %', 
+            to_char(clock_timestamp() - start_time, 'MI:SS');
+    END;
+
+<<fill_edge>>
+    DECLARE
+        start_time timestamp := clock_timestamp();
+        edge_count integer;
+    BEGIN
+        RAISE NOTICE 'FILLING CORE.EDGE';
+
+        TRUNCATE core.edge;
+        INSERT INTO core.edge (child_id, edge_type, parent_id)
+            SELECT child.node_id, template_name, parent.node_id
+            FROM pre.link
+            JOIN core.node child
+                ON child.node_id = link.child_id
+            JOIN core.node parent
+                ON parent.lang_code = parent_lang_code
+                AND parent.word = parent_word
+            JOIN core.lang ON parent.lang_code = lang.lang_code
+        ON CONFLICT DO NOTHING;
+
+    
+        edge_count := (SELECT count(*) FROM core.edge);
+        RAISE NOTICE E'\tcore.edge now has % entries', edge_count;
+        ANALYZE;
+        COMMIT;
+        RAISE NOTICE E'\tExecution time: %', 
+            to_char(clock_timestamp() - start_time, 'MI:SS');
+    END;
+
+<<fill_edges_redirect>>
+    DECLARE
+        start_time timestamp := clock_timestamp();
+        inserted_rows integer;
+    BEGIN
+        RAISE NOTICE 'FILLING CORE.EDGE WITH REDIRECTS';
+
+        INSERT INTO core.edge (child_id, edge_type, parent_id)
+            WITH redirect AS (
+                SELECT
+                    entry_id AS child_id,
+                    (regexp_match(redirect,'Reconstruction:(.+)/(.+)$'))[2] AS parent_word,
+                    (regexp_match(redirect,'Reconstruction:(.+)/(.+)$'))[1] AS parent_lang_name
+                FROM pre.entry
+                WHERE title ^@ 'Reconstruction:' AND redirect ^@ 'Reconstruction:'
+            )
+            SELECT
+                child_id,
+                'redirect',
+                parent.node_id
+            FROM redirect
+            JOIN core.node AS child ON child_id = child.node_id
+            JOIN core.lang ON lang.lang_name = redirect.parent_lang_name
+            JOIN core.node AS parent
+                ON parent.word = redirect.parent_word
+                AND parent.lang_code = lang.lang_code;
+
+        GET DIAGNOSTICS inserted_rows := ROW_COUNT;
+        RAISE NOTICE E'\tInserted % new rows into core.node (redirect)',
+            to_char(inserted_rows, '999,999,999');
+
+        CREATE INDEX ON core.edge (parent_id);
+        COMMIT;
+        RAISE NOTICE E'\tExecution time: %', 
+            to_char(clock_timestamp() - start_time, 'MI:SS');
+    END;
+
 
 <<purge_empty_languages>>
     DECLARE
