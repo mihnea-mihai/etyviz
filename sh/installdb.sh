3c82580968691ff73ef3da40580e80653cfb1de4
Mihnea Mihai <mihnea.mihai@gmx.com>
2023-10-13 23:55:10 +0300

    Fix installdb.sh

--- sh/installdb.sh
+++ sh/installdb.sh
@@ -1,3 +1,3 @@
 rm etyviz.dump
 wget https://github.com/mihnea-mihai/etyviz/releases/latest/download/etyviz.dump
-pg_restore etyviz.dump --dbname=postgres --create --clean --if-exists --no-owner
+pg_restore etyviz.dump --dbname=postgres --create --clean --if-exists --no-owner --verbose
