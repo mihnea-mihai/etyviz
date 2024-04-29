5226fc48c73679c1e422fdb509b58432cd72110f
Mihnea Mihai <mihnea.mihai@gmx.com>
2024-04-29 22:25:09 +0300

    SchemaSpy cleanup

--- sh/run_schemaspy.sh
+++
@@ -1,5 +0,0 @@
-java -jar schemaspy/schemaspy-6.2.4.jar \
- -t pgsql11 -dp schemaspy/postgresql-42.7.2.jar \
- -db etyviz -u schemaspy -p schemaspy -o docs/ -host localhost \
- -nopages \
-#  -noimplied -degree 1
